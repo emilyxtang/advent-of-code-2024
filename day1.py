ecd735a3fe04c754e5e7c8798b1e19f909bff9fd
Emily Tang <emilytang2002@gmail.com>
2024-12-01 13:32:47 -0500

    refactored day1

--- day1.py
+++ day1.py
@@ -10,18 +10,17 @@ for line in input:
     right_list.append(int(line_split[-1]))
 
 def part_one():
-    sorted_left_list = sorted(left_list)
-    sorted_right_list = sorted(right_list)
-    distance = 0
-    for i in range(len(sorted_left_list)):
-        distance += abs(sorted_left_list[i] - sorted_right_list[i])
+    distance = sum(
+        abs(left - right)
+        for left, right in zip(sorted(left_list), sorted(right_list))
+    )
     print(f'part one: {distance}')
 
 def part_two():
-    similarity_score = 0
-    for i in range(len(left_list)):
-        num = left_list[i]
-        similarity_score += num * right_list.count(num)
+    similarity_score = sum(
+        num * right_list.count(num)
+        for num in left_list
+    )
     print(f'part two: {similarity_score}')
 
 if __name__ == '__main__':
